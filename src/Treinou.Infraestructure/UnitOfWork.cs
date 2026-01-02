78324a559ecd26c43db56069dfe29381f12dc005
Nicolas de Carvalho <nicolas.carvalho@hotmail.com.br>
2026-01-02 14:39:33 -0300

    unit of work implementation

---
+++ src/Treinou.Infraestructure/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Treinou.Domain.SeedWork;
+
+namespace Treinou.Infraestructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TreinouDbContext _context;
+
+        public UnitOfWork(TreinouDbContext context)
+            => _context = context;
+
+        public async Task Commit(CancellationToken cancellationToken)
+            => await _context.SaveChangesAsync(cancellationToken);
+
+        public Task Rollback(CancellationToken cancellationToken)
+            => Task.CompletedTask;  
+    }
+}
