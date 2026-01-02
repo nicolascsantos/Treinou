2c7177c06274e50dbc0ee2c61ef56024ccd030da
Nicolas de Carvalho <nicolas.carvalho@hotmail.com.br>
2026-01-02 16:32:38 -0300

    teacher controller with get and post endpoints

---
+++ src/Treinou.API/Controllers/TeacherController.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Treinou.API.Models;
+using Treinou.Application.UseCases.Teacher.Common;
+using Treinou.Application.UseCases.Teacher.CreateTeacher;
+using Treinou.Application.UseCases.Teacher.GetTeacher;
+
+namespace Treinou.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TeacherController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpGet("{id::guid}")]
+        [ProducesResponseType(200, StatusCode = StatusCodes.Status200OK, Type = typeof(APIResponse<TeacherModelOutput>))]
+        [ProducesResponseType(404, StatusCode = StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> Get([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var output = await _mediator.Send(new GetTeacherInput(id), cancellationToken);
+            return Ok(new APIResponse<TeacherModelOutput>(output));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, StatusCode = StatusCodes.Status201Created, Type = typeof(APIResponse<TeacherModelOutput>))]
+        [ProducesResponseType(422, StatusCode = StatusCodes.Status422UnprocessableEntity, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> Create([FromBody] CreateTeacherInput input, CancellationToken cancellationToken)
+        {
+            var output = await _mediator.Send(input, cancellationToken);
+            return CreatedAtAction(nameof(Create), new { output.Id }, new APIResponse<TeacherModelOutput>(output));
+        }
+    }
+}
