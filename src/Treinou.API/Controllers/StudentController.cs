aadd1173bccdb1a90a749e0065541350754d5497
Nicolas de Carvalho <nicolas.carvalho@hotmail.com.br>
2026-01-02 15:49:36 -0300

    first database migration for entities

--- src/Treinou.API/Controllers/StudentController.cs
+++ src/Treinou.API/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Treinou.API.Models;
 using Treinou.Application.UseCases.Student.Common;
+using Treinou.Application.UseCases.Student.CreateStudent;
 using Treinou.Application.UseCases.Student.GetStudent;
 
 namespace Treinou.API.Controllers
@@ -19,10 +20,20 @@ namespace Treinou.API.Controllers
 
         [HttpGet("{id::guid}")]
         [ProducesResponseType(200, StatusCode = StatusCodes.Status200OK, Type = typeof(APIResponse<StudentModelOutput>))]
+        [ProducesResponseType(404, StatusCode = StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
         public async Task<IActionResult> Get([FromRoute] Guid id, CancellationToken cancellationToken)
         {
             var output = await _mediator.Send(new GetStudentInput(id), cancellationToken);
             return Ok(new APIResponse<StudentModelOutput>(output));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, StatusCode = StatusCodes.Status201Created, Type = typeof(APIResponse<StudentModelOutput>))]
+        [ProducesResponseType(422, StatusCode = StatusCodes.Status422UnprocessableEntity, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> Create([FromBody] CreateStudentInput input, CancellationToken cancellationToken)
+        {
+            var output = await _mediator.Send(input, cancellationToken);
+            return CreatedAtAction(nameof(Create), new { output.Id }, new APIResponse<StudentModelOutput>(output));
         }
     }
 }
