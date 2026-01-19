62ffdad08447abb4c6ba14a0635d7d64428b8bfd
Nicolas de Carvalho <nicolas.carvalho@hotmail.com.br>
2026-01-18 21:31:02 -0300

    feat: exercise api controller done

---
+++ src/Treinou.API/Controllers/ExerciseController.cs
@@ -0,0 +1,83 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Treinou.API.Models;
+using Treinou.Application.UseCases.Exercise.Common;
+using Treinou.Application.UseCases.Exercise.CreateExercise;
+using Treinou.Application.UseCases.Exercise.DeleteExercise;
+using Treinou.Application.UseCases.Exercise.GetExercise;
+using Treinou.Application.UseCases.Exercise.ListExercises;
+using Treinou.Application.UseCases.Exercise.UpdateExercise;
+using Treinou.Domain.SeedWork.SearchableRepository;
+
+namespace Treinou.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExerciseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ExerciseController(IMediator mediator)
+            => _mediator = mediator;
+
+        [HttpGet("{id::guid}")]
+        [ProducesResponseType(200, StatusCode = StatusCodes.Status200OK, Type = typeof(APIResponse<ExerciseModelOutput>))]
+        [ProducesResponseType(404, StatusCode = StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> Get([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var output = await _mediator.Send(new GetExerciseInput(id), cancellationToken);
+            return Ok(new APIResponse<ExerciseModelOutput>(output));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, StatusCode = StatusCodes.Status201Created, Type = typeof(APIResponse<ExerciseModelOutput>))]
+        [ProducesResponseType(422, StatusCode = StatusCodes.Status422UnprocessableEntity, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(409, StatusCode = StatusCodes.Status409Conflict, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> Create([FromBody] CreateExerciseInput input, CancellationToken cancellationToken)
+        {
+            var output = await _mediator.Send(input, cancellationToken);
+            return CreatedAtAction(nameof(Create), new { output.Id }, output);
+        }
+
+        [HttpDelete("{id::guid}")]
+        [ProducesResponseType(204, StatusCode = StatusCodes.Status204NoContent, Type = typeof(void))]
+        [ProducesResponseType(404, StatusCode = StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> Delete([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var output = await _mediator.Send(new DeleteExerciseInput(id), cancellationToken);
+            return NoContent();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, StatusCode = StatusCodes.Status200OK, Type = typeof(APIResponse<ListExercisesOutput>))]
+        public async Task<IActionResult> Search(
+            CancellationToken cancellationToken,
+            [FromQuery] int? page = null,
+            [FromQuery(Name = "per_page")] int? perPage = null,
+            [FromQuery] string? search = null,
+            [FromQuery] string? sort = null,
+            [FromQuery] SearchOrder? dir = null)
+        {
+            var input = new ListExercisesInput();
+
+            if (page is not null) input.Page = page.Value;
+            if (perPage is not null) input.PerPage = perPage.Value;
+            if (!string.IsNullOrWhiteSpace(search)) input.Search = search;
+            if (!string.IsNullOrWhiteSpace(sort)) input.Sort = sort;
+            if (dir is not null) input.Dir = dir.Value;
+
+            var output = await _mediator.Send(input, cancellationToken);
+
+            return Ok(new APIResponseList<ExerciseModelOutput>(output));
+        }
+
+        [HttpPut]
+        [ProducesResponseType(200, StatusCode = StatusCodes.Status200OK, Type = typeof(APIResponse<ExerciseModelOutput>))]
+        [ProducesResponseType(404, StatusCode = StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> Update([FromBody] UpdateExerciseInput input, CancellationToken cancellationToken)
+        {
+            var output = await _mediator.Send(input, cancellationToken);
+            return Ok(new APIResponse<ExerciseModelOutput>(output));
+        }
+    }
+}
