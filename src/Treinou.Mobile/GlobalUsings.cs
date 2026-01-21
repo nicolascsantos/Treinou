432dc446bf96120bb2b9ca71672e6afb0c0981b7
Nicolas de Carvalho <nicolas.carvalho@hotmail.com.br>
2026-01-21 18:07:29 -0300

    removing maui project for replacement

--- src/Treinou.Mobile/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using Treinou.Mobile.Data;
-global using Treinou.Mobile.PageModels;
-global using Treinou.Mobile.Pages;
-global using Treinou.Mobile.Services;
-global using Treinou.Mobile.Utilities;
