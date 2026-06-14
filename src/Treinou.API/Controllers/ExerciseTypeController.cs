223bea6617b7d98518b1a0a288c89d743fb21e67
Nicolas de Carvalho Santos <nicolas.carvalho@hotmail.com.br>
2026-06-14 00:36:56 -0300

    fix: ExerciseTypeController Update dispatches correct use case

--- src/Treinou.API/Controllers/ExerciseTypeController.cs
+++ src/Treinou.API/Controllers/ExerciseTypeController.cs
@@ -6,8 +6,7 @@ using Treinou.Application.UseCases.ExerciseType.CreateExerciseType;
 using Treinou.Application.UseCases.ExerciseType.DeleteExerciseType;
 using Treinou.Application.UseCases.ExerciseType.GetExerciseType;
 using Treinou.Application.UseCases.ExerciseType.ListExerciseTypes;
-using Treinou.Application.UseCases.Student.Common;
-using Treinou.Application.UseCases.Student.UpdateStudent;
+using Treinou.Application.UseCases.ExerciseType.UpdateExerciseType;
 using Treinou.Domain.SeedWork.SearchableRepository;
 
 namespace Treinou.API.Controllers
@@ -75,12 +74,12 @@ namespace Treinou.API.Controllers
         }
 
         [HttpPut]
-        [ProducesResponseType(200, StatusCode = StatusCodes.Status200OK, Type = typeof(APIResponse<StudentModelOutput>))]
+        [ProducesResponseType(200, StatusCode = StatusCodes.Status200OK, Type = typeof(APIResponse<ExerciseTypeModelOutput>))]
         [ProducesResponseType(404, StatusCode = StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
-        public async Task<IActionResult> Update([FromBody] UpdateStudentInput input, CancellationToken cancellationToken)
+        public async Task<IActionResult> Update([FromBody] UpdateExerciseTypeInput input, CancellationToken cancellationToken)
         {
             var output = await _mediator.Send(input, cancellationToken);
-            return Ok(new APIResponse<StudentModelOutput>(output));
+            return Ok(new APIResponse<ExerciseTypeModelOutput>(output));
         }
     }
 }
