8012b3e09510f444a15e95561157b2265e3c83b7
Nicolas de Carvalho Santos <nicolas.carvalho@hotmail.com.br>
2026-07-05 01:07:27 -0300

    adicionando cors local

--- src/Treinou.API/Program.cs
+++ src/Treinou.API/Program.cs
@@ -2,11 +2,23 @@ using Treinou.API.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200/")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddAndConfigureControllers();
 builder.Services.AddUseCases();
 builder.Services.AddAppConnections(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseCors("AllowAngularDev");
 
 app.UseDocumentation();
 
