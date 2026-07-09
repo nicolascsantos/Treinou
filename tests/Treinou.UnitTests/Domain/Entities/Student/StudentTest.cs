e56f174bedba2008e3fb27dece04939ea56f7d7d
Nicolas de Carvalho Santos <nicolas.carvalho@hotmail.com.br>
2026-07-09 18:54:22 -0300

    Student instantiate test

---
+++ tests/Treinou.UnitTests/Domain/Entities/Student/StudentTest.cs
@@ -0,0 +1,45 @@
+﻿using DomainEntities = Treinou.Domain.Entities;
+
+namespace Treinou.UnitTests.Domain.Entities.Student
+{
+    [Collection(nameof(StudentTestFixture))]
+    public class StudentTest
+    {
+        private readonly StudentTestFixture _fixture;
+
+        public StudentTest(StudentTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact(DisplayName = nameof(Instantiate))]
+        [Trait("Domain", "Student - Aggregates")]
+        public void Instantiate()
+        {
+            var validStudent = _fixture.GetValidStudent();
+
+            var dateTimeBefore = DateTime.Now;
+
+            var newStudent = new DomainEntities.Student
+            (
+                validStudent.Name,
+                validStudent.Email,
+                validStudent.CPF,
+                validStudent.PhoneNumber,
+                validStudent.Weight,
+                validStudent.Height,
+                validStudent.IsActive
+            );
+
+            var dateTimeAfter = DateTime.Now;
+
+            Assert.Equal(newStudent.Name, validStudent.Name);
+            Assert.Equal(newStudent.Email, validStudent.Email);
+            Assert.Equal(newStudent.CPF, validStudent.CPF);
+            Assert.Equal(newStudent.PhoneNumber, validStudent.PhoneNumber);
+            Assert.Equal(newStudent.Weight, validStudent.Weight);
+            Assert.Equal(newStudent.Weight, validStudent.Weight);
+            Assert.Equal(newStudent.IsActive, validStudent.IsActive);
+        }
+    }
+}
---
+++ tests/Treinou.UnitTests/Domain/Entities/Student/StudentTestFixture.cs
@@ -0,0 +1,54 @@
+﻿using Treinou.Domain.ValueObjects;
+using Treinou.UnitTests.Common;
+using DomainEntities = Treinou.Domain.Entities;
+
+namespace Treinou.UnitTests.Domain.Entities.Student
+{
+    [CollectionDefinition(nameof(StudentTestFixture))]
+    public class StudentTestFixtureCollection : ICollectionFixture<StudentTestFixture> { }
+    public class StudentTestFixture : BaseFixture
+    {
+        public DomainEntities.Student GetValidStudent()
+            => new(
+                GetValidName(),
+                GetValidEmail(),
+                GetValidCPF(),
+                GetValidPhoneNumber(),
+                GetValidWeight(),
+                GetValidHeight(),
+                GetRandomBoolean()
+            );
+
+        public string GetValidName() => Faker.Name.FullName();
+
+        public Email GetValidEmail() => new(Faker.Person.Email);
+
+        public CPF GetValidCPF() => new("10223229075");
+
+        public PhoneNumber GetValidPhoneNumber() => new("13999999999");
+
+        public double GetValidWeight()
+        {
+            Random random = new();
+            double minimumWight = 45.0;
+            double maximumWeight = 120.0;
+
+            double finalWeight = minimumWight + (random.NextDouble() * (maximumWeight - minimumWight));
+
+            return Math.Round(finalWeight, 2);
+        }
+
+        public static double GetValidHeight()
+        {
+            Random random = new();
+            double minimumHeight = 1.50;
+            double maximumHeight = 2.10;
+
+            double finalHeight = minimumHeight + (random.NextDouble() * (maximumHeight - minimumHeight));
+
+            return Math.Round(finalHeight, 2);
+        }
+
+        public bool GetRandomBoolean() => new Random().NextDouble() < 0.5;
+    }
+}
