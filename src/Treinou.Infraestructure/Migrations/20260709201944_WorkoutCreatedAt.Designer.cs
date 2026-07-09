6a87b2e1f8a890bc88cc9f7200376636532569d1
Nicolas de Carvalho Santos <nicolas.carvalho@hotmail.com.br>
2026-07-09 18:14:51 -0300

    adicionando createdat em workout

--- src/Treinou.Infraestructure/Migrations/20260709201944_WorkoutCreatedAt.Designer.cs
+++ src/Treinou.Infraestructure/Migrations/20260709201944_WorkoutCreatedAt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Treinou.Infraestructure;
 
@@ -11,9 +12,11 @@ using Treinou.Infraestructure;
 namespace Treinou.Infraestructure.Migrations
 {
     [DbContext(typeof(TreinouDbContext))]
-    partial class TreinouDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260709201944_WorkoutCreatedAt")]
+    partial class WorkoutCreatedAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
