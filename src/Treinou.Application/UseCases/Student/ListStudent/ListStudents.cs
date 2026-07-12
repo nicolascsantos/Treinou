f54f743291bf4f83d4a73c6bd2950b80b8350e87
Nicolas de Carvalho Santos <nicolas.carvalho@hotmail.com.br>
2026-07-12 18:40:27 -0300

    perf: remove N+1 teacher-fetch loop from ListStudents

--- src/Treinou.Application/UseCases/Student/ListStudent/ListStudents.cs
+++ src/Treinou.Application/UseCases/Student/ListStudent/ListStudents.cs
@@ -5,29 +5,16 @@ namespace Treinou.Application.UseCases.Student.ListStudent
     public class ListStudents : IListStudents
     {
         private readonly IStudentRepository _studentRepository;
-        private readonly ITeacherRepository _teacherRepository;
 
-        public ListStudents(IStudentRepository studentRepository, ITeacherRepository teacherRepository)
+        public ListStudents(IStudentRepository studentRepository)
         {
             _studentRepository = studentRepository;
-            _teacherRepository = teacherRepository;
         }
 
         public async Task<ListStudentsOutput> Handle(ListStudentsInput request, CancellationToken cancellationToken)
         {
             var searchOutput = await _studentRepository.Search(request.ToSearchInput(), cancellationToken);
-
-            foreach (var student in searchOutput.Items)
-            {
-                if (student.TeacherId != default)
-                {
-                    var teacher = await _teacherRepository.Get(student.TeacherId ?? default, cancellationToken);
-                    student.Teacher = teacher;
-                }
-            }
-
             var output = ListStudentsOutput.FromSearchOutput(searchOutput);
-
             return output;
         }
     }
