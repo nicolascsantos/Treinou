e4734a13b6378ddf9fcde7863b52ff912ad8576a
Nicolas de Carvalho Santos <nicolas.carvalho@hotmail.com.br>
2026-07-12 19:14:40 -0300

    fix: remove trailing slash from CORS allowed origin

--- src/Treinou.API/Program.cs
+++ src/Treinou.API/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularDev", policy =>
     {
-        policy.WithOrigins("http://localhost:4200/")
+        policy.WithOrigins("http://localhost:4200")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
